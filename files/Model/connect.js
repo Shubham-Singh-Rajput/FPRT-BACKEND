f4962bec6cf6fdaa5e76306ec4048c68d6f59793
shubham singh <shubham.singh7705@gmail.com>
2021-07-11 23:20:37 +0530

    heroku

--- files/Model/connect.js
+++ files/Model/connect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const connect=async()=>{
     try{
-        const uri='mongodb://localhost:27017/FPRT'
+        const uri="mongodb+srv://shubham-72:mondob1@cluster0.cieyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
         await mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
         console.log('connected')
     }
@@ -10,3 +10,4 @@ const connect=async()=>{
     }
 }
 export default connect
+// 'mongodb://localhost:27017/FPRT'
